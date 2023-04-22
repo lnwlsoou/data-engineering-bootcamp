45cc7162b71c4e67dba81b621f7e1f182565a3ba
Sopa Potikanya <sopapotikanya@hotmail.com>
2023-04-22 10:18:25 +0000

    day1

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/8b247c12-31a2-44e8-8308-17f459527e47/basket/randomDogs"
